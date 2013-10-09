8f11508681a485802720ba1e5a98d52ad1ca65c3
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-09 14:36:45 -0400

    Putting information on 2 lines for first prompt

--- Prompt and Alert /js/script.js
+++ Prompt and Alert /js/script.js
@@ -1,9 +1,12 @@
 //Prompt and Alert
 
-var userInput = prompt("Enter your year of birth:"); 
-console.log(userInput);
+//var userInput = prompt("Enter your year of birth:"); 
+//console.log(userInput);
 
-
+ var width = prompt("We are calculating the area of a rectangle.  \nPlease enter the width.");
+ var height = prompt("Please enter the height.");
+ 
+ 
 
 
 

47f898471451250edaa4b86145ac182df1a367e8
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-09 11:27:22 -0400

    update number of people at the party

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -15,8 +15,11 @@ console.log("Sparky is" +" "+ sparkyAge +" "+ "human years old which is" +" "+ d
 //Slice of Pie part 1
 
 var pizzaSlices = 10;
-var partyPeople = 15;
+var partyPeople = 20;
 var pizzas = 5;
 
 //multiply pizzaSlices and pizzas then divide by partyPeople & assign to slicesPerPerson
 var slicesPerPerson = pizzaSlices * pizzas / partyPeople;
+
+//print out to the output slices per person
+console.log("Each person ate" +" "+ slicesPerPerson +" "+ "slices of pizza at the party.");

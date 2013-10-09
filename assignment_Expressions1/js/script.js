0a2b1dcc7c5fc7ff9716443a9c507f3792f1d662
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-08 21:44:25 -0400

    concatenating

--- assignment_Expressions1/js/script.js
+++ assignment_Expressions1/js/script.js
@@ -1,9 +1,5 @@
-//Expressions - Expressions with Arrays
+//Expressions - Concatenating
 
-var orangeBins = [234, 567, 883];
-
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-console.log(total);
 
 
 

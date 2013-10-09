c51f1b25dfad7da87707112d415b0c8ce25a6aa4
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-09 10:55:00 -0400

    delete example problem

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,12 +1,2 @@
 //Janell Stultz 10/9/13 Expressions Worksheet
 
-//Calculate the Area of a Rectangle 
-//Givens
-var length = 5; 
-var width = 3;
-
-//Result variable _ multiply length times width to determine the area
-var area = length * width;
-
-//Result to Print _ print out the output area
-console.log("The area of the rectangle is" +" "+ area +" "+ "feet.");

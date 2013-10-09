deaa997ca28cf9f90885d7341a753bcf29c5c8ca
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-08 21:43:35 -0400

    expressions with arrays

--- assignment_Expressions1/js/script.js
+++ assignment_Expressions1/js/script.js
@@ -1,17 +1,9 @@
-//Expressions - Order of Operations
+//Expressions - Expressions with Arrays
 
-var quiz1 = 87;
-var quiz2 = 100;
-var quiz3 = 60;
-var quiz4 = 80;
+var orangeBins = [234, 567, 883];
 
-//the averages is the sum of the items divided by the number of items
-var average = (quiz1 + quiz2 + quiz3 + quiz4)/4
-//console.log(average);
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
 
-var length = 7;
-var width = 6;
-var perimeter = length * 2 + width * 2;
-console.log(perimeter);
 
-//pay attention to order of operations to know when to add parenthesis
+

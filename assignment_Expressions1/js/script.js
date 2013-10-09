2aa53212cb26c9f0dc2ffd8ff424dbfa7d382326
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-08 21:48:28 -0400

    var result

--- assignment_Expressions1/js/script.js
+++ assignment_Expressions1/js/script.js
@@ -1,10 +1,10 @@
 //Expressions - Concatenating Strings
 
-var firstName = "Janell";
-var lastName = "Stultz";
-var fullName = firstName + " " + lastName;
- 
-console.log(fullName);
+var a = "6";
+var b = "7";
+
+var result = a + b;
+console.log(result);
 
 
 

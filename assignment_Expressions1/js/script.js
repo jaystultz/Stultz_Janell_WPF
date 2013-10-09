7f3f454924df8998d1bfda512d827fe9f0706201
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-08 21:35:20 -0400

    add vars

--- assignment_Expressions1/js/script.js
+++ assignment_Expressions1/js/script.js
@@ -1,10 +1,11 @@
 //Expressions - Order of Operations
 
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
 
-
-
-
-
+//the averages is the sum of the items divided by the number of items
 
 
 

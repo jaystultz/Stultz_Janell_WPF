262713a901e90eec58ef598dc963bd41eb9749c3
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-08 21:54:34 -0400

    add new var

--- assignment_Expressions1/js/script.js
+++ assignment_Expressions1/js/script.js
@@ -1,5 +1,8 @@
 //Expressions - Casting
 
-var stringVar = "6";
-var result = 7+ Number(stringVar);
-console.log(result);
+var areaCode = 407;
+var firstPart = 203;
+var secPart = 4529;
+
+var phoneNo = areaCode + firstPart + secPart;
+console.log(phoneNo);

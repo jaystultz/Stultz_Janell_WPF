d8f7777ffe545b8497d68a75df8138c9c19837d7
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-08 22:19:34 -0400

    casting string

--- assignment_Expressions1/js/script.js
+++ assignment_Expressions1/js/script.js
@@ -4,5 +4,5 @@ var areaCode = 407;
 var firstPart = 203;
 var secPart = 4529;
 
-var phoneNo = areaCode + firstPart + secPart;
+var phoneNo = String(areaCode) + String(firstPart) + String(secPart);
 console.log(phoneNo);

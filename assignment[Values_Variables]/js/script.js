309a057986c76c712e7ea380aaec5386885b2b33
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-08 20:41:05 -0400

    incorrect use of single quote

--- assignment[Values_Variables]/js/script.js
+++ assignment[Values_Variables]/js/script.js
@@ -2,7 +2,8 @@
 
 var age = 31;
 
-var isStudent = false;
+var isStudent = false;  //Boolean variable
 
-var firstName = "Janell";
+var firstName = "Janell";  //string variable
 
+var phrase = 'I don't know';

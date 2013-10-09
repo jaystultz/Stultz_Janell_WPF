34e5f4a503360b702cf9b6e4c242ad7f6e03f4ef
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-09 14:37:35 -0400

    add var area and print to console

--- Prompt and Alert /js/script.js
+++ Prompt and Alert /js/script.js
@@ -5,8 +5,9 @@
 
  var width = prompt("We are calculating the area of a rectangle.  \nPlease enter the width.");
  var height = prompt("Please enter the height.");
- 
- 
+ var area = width * height;
+ console.log(area);
+  
 
 
 

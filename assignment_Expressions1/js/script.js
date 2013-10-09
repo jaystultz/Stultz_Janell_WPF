de73430876c9016fba02a98032ddf5fa26e50bc1
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-08 21:19:18 -0400

    Expression age example

--- assignment_Expressions1/js/script.js
+++ assignment_Expressions1/js/script.js
@@ -1,9 +1,6 @@
-//Expressions
+//Expressions - Age Example
 
-var a = 2; //sets up our variable a and defines it with 2 
-var b;
-b = a + 3; //add 3 to a
-console.log(b);
+var yearBorn = 1982;
 
 
 

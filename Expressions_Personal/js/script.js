0095c0d9fe3af1defa6f59f63e533311256d15f8
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-10 10:33:01 -0400

    add personal, industry and wacky folders with HTML, images, CSS and JS assets

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,10 @@
+//Janell Stultz 10/10/13 Expressions_Personal
+
+ var width = prompt("We are calculating the area of a rectangle.  \nPlease enter the width.");
+ var height = prompt("Please enter the height.");
+ var area = width * height; //calculates with that info
+ //console.log(area); //prints it out to the console
+var result = "The area of your rectangle is " + area + " sq feet";
+alert(result);
+ 
+var 

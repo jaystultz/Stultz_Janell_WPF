a13ffa57350d627398cf0ed41a3075cad5d2d10e
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-10 21:17:36 -0400

    add var cookies with prompt info and note

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,5 @@
 //Janell Stultz 10/10/13 Expressions_Wacky
 
 //Calculating how many miles needed to run in order to burn off calories from milk, cookies and ice cream.
+
+var cookies = prompt("We are calculating how many miles needed to run in order to burn off calories from cookies, milk and ice cream.  \nPlease enter how many cookies you ate."); //how many cookies a person ate.

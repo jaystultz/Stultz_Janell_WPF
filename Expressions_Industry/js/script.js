efff9e459721f64f60908bc35a91971e10b83b3a
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-10 21:09:37 -0400

    add var thursday with prompt info and note

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,3 +4,4 @@
 var monday = prompt("We are calculating your total hours per week and average hours per day spent coding.  \nPlease enter enter your hours for Monday."); //this var shows how many hours were spent coding on Monday.
 var tuesday = prompt("Please enter enter your hours for Tuesday."); //this var shows how many hours were spent coding on Tuesday.
 var wednesday = prompt("Please enter enter your hours for Wednesday."); //this var shows how many hours were spent coding on Wednesday.
+var thursday = prompt("Please enter enter your hours for Thursday.");//this var shows how many hours were spent coding on Thursday.

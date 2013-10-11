8b1515ac2a56aa6dc3946eafe64b259ffd7baa8c
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-10 21:18:11 -0400

    add var iceCream with prompt info and note

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,3 +4,4 @@
 
 var cookies = prompt("We are calculating how many miles needed to run in order to burn off calories from cookies, milk and ice cream.  \nPlease enter how many cookies you ate."); //how many cookies a person ate.
 var milk = prompt("Please enter how many 8 oz. glasses of milk you drank."); //how many glasses of milk a person drank.
+var iceCream = prompt("Please enter how many scoops of ice cream you ate."); //how many scoops of ice cream a person ate.

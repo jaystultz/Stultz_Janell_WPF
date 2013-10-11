74ccfe562d5d41f84a359970c7eaa648cc0d29ff
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-10 21:17:54 -0400

    add var milk with prompt info and note

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,3 +3,4 @@
 //Calculating how many miles needed to run in order to burn off calories from milk, cookies and ice cream.
 
 var cookies = prompt("We are calculating how many miles needed to run in order to burn off calories from cookies, milk and ice cream.  \nPlease enter how many cookies you ate."); //how many cookies a person ate.
+var milk = prompt("Please enter how many 8 oz. glasses of milk you drank."); //how many glasses of milk a person drank.

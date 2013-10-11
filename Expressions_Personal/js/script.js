655aeb5d4e21d88e3d8e0eda2eab8d5dfe31e8a1
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-10 20:52:04 -0400

    add var height with instructions and note

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,4 +2,5 @@
 
 //Calculating a woman's BMR
 var weight = prompt("We are calculating the Basal Metabolic Rate for a woman.  \nPlease enter your weight in kilograms.");
+var height = prompt("Please enter your height in centimeters."); //this var collects the woman's height.
 

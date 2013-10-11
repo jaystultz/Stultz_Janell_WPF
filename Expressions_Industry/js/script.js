cf65265e9aa8367fd614332c689255fffe87e32d
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-10 21:13:47 -0400

    add var result

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -12,3 +12,4 @@ var totalHours = Number(monday) + Number(tuesday) + Number(wednesday) + Number(t
 console.log(totalHours); //prints total hours to console.
 var averageHours = totalHours / 5; //Dividing the total amount of hours spent coding by the number of days worked shows the average hours per day that were spent coding each day.  
 console.log(averageHours); //prints average to the console.
+var result = "You worked a total of" +" "+ totalHours +" "+ "this week.  You averaged" +" "+ averageHours +" "+ "hours per day.";  //The result of the calculation.

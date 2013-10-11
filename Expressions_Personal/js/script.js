ddae8c3d8cd7f441fd325e7bb568a26063f5cd94
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-10 20:54:28 -0400

    add result alert with note

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,4 +7,5 @@ var age = prompt("Please enter your age in years."); //this var collects the wom
 var bmr= 10 * weight + 6.25 * height - 5 * age; //multiply through then add or subtract each product to determine ideal daily caloric intake.
 console.log(bmr); //prints total out to the console
 var result = "Your BMR is" +" "+ bmr +" "+ "calories per day"; //this var shows the woman's BMR.
+alert(result); //produces an alert for the result of the calculation.
 

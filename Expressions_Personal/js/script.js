ce57fd2cec73ccd293f6b2f63ec4a951bc7a2460
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-10 20:52:29 -0400

    add var age prompt with instructions and note

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,4 +3,5 @@
 //Calculating a woman's BMR
 var weight = prompt("We are calculating the Basal Metabolic Rate for a woman.  \nPlease enter your weight in kilograms.");
 var height = prompt("Please enter your height in centimeters."); //this var collects the woman's height.
+var age = prompt("Please enter your age in years."); //this var collects the woman's age.
 

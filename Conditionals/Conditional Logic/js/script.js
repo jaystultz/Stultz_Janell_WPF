3519dcc1716c22c81b7eba5e3a5a95a14293cab4
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 13:57:51 -0400

    add statement

--- Conditionals/Conditional Logic/js/script.js
+++ Conditionals/Conditional Logic/js/script.js
@@ -22,8 +22,8 @@
 var budget = 300;
 var iPhonePrice = 199.99;
 var paycheck = 200;
-
-if(){
+//if the price of the phone is less than our budget
+if(iPhonePrice < budget){
 	console.log("We can buy the phone!");
 }else{
 	console.log("No phone for you!!");

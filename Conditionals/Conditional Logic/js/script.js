1652ba50680245eeb61a07ee504a1a249bffb457
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 13:55:18 -0400

    comment out previous work and add logical operators

--- Conditionals/Conditional Logic/js/script.js
+++ Conditionals/Conditional Logic/js/script.js
@@ -1,21 +1,24 @@
 //Conditional Logic - else if
 
-var kidHeight = 52;
-var minHeight = 48;
-var wParentHeight = 45; //the height of the kid with the parent
+//var kidHeight = 52;
+//var minHeight = 48;
+//var wParentHeight = 45; //the height of the kid with the parent
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+//if(kidHeight > minHeight){
 	//you can ride!
-	console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight){
+	//console.log("You can ride the coaster!");
+//}else if(kidHeight > wParentHeight){
 	//you can ride with a parent present
-	console.log("You can ride, but only with a parent present.");
-}else{
+	//console.log("You can ride, but only with a parent present.");
+//}else{
 	//sorry you have growing to do
-	console.log("Sorry kid, you've got some growing to do first.");
+	//console.log("Sorry kid, you've got some growing to do first.");
 }
+
+
+//Logical operators
 
 
 

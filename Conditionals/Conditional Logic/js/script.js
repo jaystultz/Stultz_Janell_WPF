7f9d6bfeac472ad99db0a2d6b15fa17339f74e3b
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 13:27:36 -0400

    remove sneakerLift var

--- Conditionals/Conditional Logic/js/script.js
+++ Conditionals/Conditional Logic/js/script.js
@@ -2,11 +2,10 @@
 
 var kidHeight = 47;
 var minHeight = 48;
-var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!")
 }

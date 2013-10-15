49b9a98c21abd4cf6d38dadd3d04263d9787343e
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 14:30:57 -0400

    hide statement

--- Conditionals/Conditional Logic/js/script.js
+++ Conditionals/Conditional Logic/js/script.js
@@ -22,9 +22,10 @@ var age = 11;
 var book;
 
 //If the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
-if (age<10){
+/*if (age<10){
 	book = "Green Eggs and Ham";
 }else{
 	book = "The Time Machine";
 }
-console.log(book);
+console.log(book);*/
+

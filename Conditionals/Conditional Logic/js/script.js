6ebae40aa5a6ba07ca0e37d31a74ae1a11c6e1fd
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 13:39:50 -0400

    add var wParentHeight and also add else if statement

--- Conditionals/Conditional Logic/js/script.js
+++ Conditionals/Conditional Logic/js/script.js
@@ -2,13 +2,14 @@
 
 var kidHeight = 52;
 var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight){
 	//you can ride!
 	console.log("You can ride the coaster!");
-}else if(){
+}else if(kidHeight > wParentHeight){
 	//you can ride with a parent present
 	console.log("You can ride, but only with a parent present.");
 }else{

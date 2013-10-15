086f1abca2ca285c322dbeb9770acce0fd4cc415
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 13:32:48 -0400

    add "else"

--- Conditionals/Conditional Logic/js/script.js
+++ Conditionals/Conditional Logic/js/script.js
@@ -8,8 +8,7 @@ var minHeight = 48;
 if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!")
-}
-if(kidHeight <= minHeight){
+}else{
 	console.log("Sorry kid, you've got some growing to do first.");
 }
 

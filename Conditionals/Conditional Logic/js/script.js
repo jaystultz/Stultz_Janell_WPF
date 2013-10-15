4d70b78e30fedf3b86b93ba8d0841f81f3afb75b
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 14:10:40 -0400

    remove true

--- Conditionals/Conditional Logic/js/script.js
+++ Conditionals/Conditional Logic/js/script.js
@@ -4,7 +4,7 @@ var iPhonePrice = 199.99;
 var wonLottery = true;
 
  //if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget || wonLottery === true){
+if(iPhonePrice < budget || wonLottery){
 	console.log("We can buy the phone!");
 }else{
 	console.log("No phone for you!!");

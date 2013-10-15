b35d3787c88c2eda04dc325711d5870e228e6514
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 14:07:51 -0400

    change variable

--- Conditionals/Conditional Logic/js/script.js
+++ Conditionals/Conditional Logic/js/script.js
@@ -1,7 +1,8 @@
 //Conditional Logic - Logical operators
 var budget = 300;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
+
 //if the price of the phone is less than our budget AND if our paycheck is over 300
 if(iPhonePrice < budget && paycheck > 300){
 	console.log("We can buy the phone!");

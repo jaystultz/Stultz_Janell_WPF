7805d57bdb531420d2817453e08398948c77e23f
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 13:38:31 -0400

    add else if statement

--- Conditionals/Conditional Logic/js/script.js
+++ Conditionals/Conditional Logic/js/script.js
@@ -6,10 +6,13 @@ var minHeight = 48;
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight){
-	//code performed if condition is true
-	console.log("You can ride the coaster!")
+	//you can ride!
+	console.log("You can ride the coaster!");
+}else if(){
+	//you can ride with a parent present
+	console.log("You can ride, but only with a parent present.");
 }else{
-	//code performed if condition is false
+	//sorry you have growing to do
 	console.log("Sorry kid, you've got some growing to do first.");
 }
 

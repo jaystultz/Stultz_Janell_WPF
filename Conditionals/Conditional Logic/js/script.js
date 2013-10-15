1283d18f394900c0261283f3ecf166b6a7711a5b
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 14:01:58 -0400

    add && statement

--- Conditionals/Conditional Logic/js/script.js
+++ Conditionals/Conditional Logic/js/script.js
@@ -3,12 +3,7 @@ var budget = 300;
 var iPhonePrice = 199.99;
 var paycheck = 200;
 //if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget){
-	console.log("We can buy the phone!");
-}else{
-	console.log("No phone for you!!");
-}
-if(paycheck > 300){
+if(iPhonePrice < budget && paycheck > 300){
 	console.log("We can buy the phone!");
 }else{
 	console.log("No phone for you!!");

a26c7eb315d9d05d2f9542f9497a965d0c8b2724
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 13:14:54 -0400

    add conditionals folder with formula and examples file

---
+++ Conditionals/Conditionals Formula and Examples/script.js
@@ -0,0 +1,47 @@
+//conditionals screencast
+//Conditional Statements video 1
+//skeleton formula for conditionals
+if(condition){
+	actions
+}	
+
+//example if conditional is true
+if(sunny){
+	goToTheBeach();
+}
+
+//example if conditional is false and there is an alternative option
+if(sunny){
+	goToTheBeach();
+}else{
+	goToTheMovies();
+}
+
+//video 2
+//example of specific conditional
+if(temp>70){
+	goToTheBeach();
+}
+
+//example of specific temperature
+if(temp==75){
+	goToTheBeach();
+}
+
+
+//Nesting conditionals - video 3
+ if(sunny){
+ 	goToTheBeach();
+ 	
+ 	if(warmWater){
+ 		wearNewSuit();
+ 	}
+ 
+ }else{
+ 	goToTheMovies();
+ 
+ 
+ 
+ 
+ 
+ 

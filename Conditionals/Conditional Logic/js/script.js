736314bbfce7d8c56b7ee86b7a3dce8b6542c5aa
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-15 14:08:41 -0400

    update if statement

--- Conditionals/Conditional Logic/js/script.js
+++ Conditionals/Conditional Logic/js/script.js
@@ -3,8 +3,8 @@ var budget = 300;
 var iPhonePrice = 199.99;
 var wonLottery = true;
 
-//if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget && paycheck > 300){
+ //if the price of the phone is less than our budget AND if our paycheck is over 300
+if(iPhonePrice < budget || wonLottery === true){
 	console.log("We can buy the phone!");
 }else{
 	console.log("No phone for you!!");

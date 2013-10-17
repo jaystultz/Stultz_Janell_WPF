fc197aaba73e02af323bd51c2dea58aae46c5dd1
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-17 01:25:24 -0400

    add printout and alert

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -9,3 +9,5 @@ var reservation = prompt("Please enter your number of reservations."); //this va
 var time = prompt("Please enter the current time to see if there are any rooms available.  \nPlease use military time format."); //this variable represents the current time in military time format.
 var checkInTime = 1500; //this variable represents the minimum time that check in is allowed.
 if(reservation === ""){ //this if statement contains the validation statement for the reservation variable meaning that this statement checks to ensure that the variable is not left blank.  If it is blank, an error alert is produced.
+	console.log("Please enter a valid amount of reservations and current time."); //this is the printout of the error alert if the reservation variable is left blank.
+	alert("Please enter a valid amount of reservations and/or current time."); //this is the alert if the reservation variable is left blank.

05d8c06871e0db1dcf49e6ff048a55065982d354
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-17 01:10:21 -0400

    add printout and alert regarding empty input

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -11,3 +11,5 @@ if(clients === ""){ //this if statement contains the validation statement for th
 	console.log("Please enter valid amounts."); //this is the printout of the error alert if the clients variable is left blank.
 	alert("Please enter valid amounts."); //this is the alert if the clients variable is left blank.
 }else if(projects === ""){ //this else if statement contains the validation statement for the projects variable meaning that this statement checks to ensure that the variable is not left blank.  If it is blank, an error alert is produced.
+	console.log("Please enter valid amounts."); //this is the printout of the error alert if the projects variable is left blank.
+	alert("Please enter valid amounts."); //this is alert if the projects variable is left blank.

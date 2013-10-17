9729039e8ab8a28ea0d6dbdcdfad26f8bd815fce
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-17 01:07:51 -0400

    add "if" statement with variable validation

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -7,3 +7,4 @@ var clients = prompt("How many clients have you signed?"); //this variable repre
 var projects = prompt("How many projects have you completed?"); //this variable represents the number of projects the employee has successfully completed.
 var clientQuota = 5; //this variable represents the minimum number of clients signed that is required for the employee to receive a promotion.
 var minimumProjects = 7; //this variable represents the minimum number of completed projects that is required for the employee to receive a promotion.
+if(clients === ""){ //this if statement contains the validation statement for the clients variable meaning that this statement checks to ensure that the variable is not left blank.  If it is blank, an error alert is produced.

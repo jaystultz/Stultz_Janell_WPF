21e7873516334966d83482b645c504da21c24c2d
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-17 01:23:56 -0400

    add conditional statements

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,3 +1,5 @@
 //Janell Stultz 10/17/2013 - Assignment: Conditionals_Wacky
 
-
+//If you have a reservation and it is after 3pm, then you may check into your hotel room.  
+//If you have a reservation but it is before 3pm, you must wait until after 3pm to check in.
+//If you do not have a reservation, regardless of what time it is, you need to see the front desk for assistance.

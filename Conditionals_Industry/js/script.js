6e2337012632f82a8aa23c09146c21cb9be207fa
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-17 01:07:12 -0400

    add conditional statement and givens

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,2 +1,9 @@
 //Janell Stultz 10/16/2013 - Assignment: Conditionals_Industry
 
+//If I sign > the client quota and I complete > minimum projects, I will get a promotion.  If I do not complete one or both of these requirements, I will not get a promotion.
+
+//givens
+var clients = prompt("How many clients have you signed?"); //this variable represents the number of clients the employee has signed.
+var projects = prompt("How many projects have you completed?"); //this variable represents the number of projects the employee has successfully completed.
+var clientQuota = 5; //this variable represents the minimum number of clients signed that is required for the employee to receive a promotion.
+var minimumProjects = 7; //this variable represents the minimum number of completed projects that is required for the employee to receive a promotion.

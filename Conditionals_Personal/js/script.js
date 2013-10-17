0d2cb77a8f400cd3f7336193a6b883164647c3b9
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-17 01:19:20 -0400

    add variables

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,3 +2,7 @@
 
 //If I make use the bread that is left sandwiches for my children, then will there be enough bread left to make one for me?
 
+//givens
+var breadTotal = prompt("We are calculating if there will be enough bread left over to make a sandwich for yourself after making sandwiches for your children.  \nPlease enter total amount of bread slices you have."); //this variable represents the number of bread slices I have.
+var sandwiches = prompt("How many sandwiches do you need to make for your children?"); //this variable represents the total amount of sandwiches that I need to make for my children.
+var breadForSandwich = 2; //this variable represents how many pieces of bread is needed for one sandwich.

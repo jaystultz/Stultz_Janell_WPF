344a024e7df744ed5ee285a63f811cfc61c84b01
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-20 00:35:34 -0400

    add the return element to collect the answer to the function

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -5,4 +5,5 @@
 	//basic skeleton set up for an anonymous function
 var circleCirc = function(radius){	//replace functionName with proper name for the function & enter the proper parameter 
 	var circ = radius * 2 * 3.14;  //this variable represents the formula for finding the circumference of a circle.  The "2" and "3.14" are constant values.
+	return circ;  //the return element retrieves and spits out the information (answer) for this function.  This value will not be stored using the return element.
 }

deba43e7c00f9af41bcf079657d28962698b75b5
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-20 00:31:29 -0400

    enter the appropriate parameter for this problem

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -3,6 +3,6 @@
 //Circumference - calculate the circumference of a circle
 
 	//basic skeleton set up for an anonymous function
-var circleCirc = function(parameter){	//replace functionName with proper name for the function
+var circleCirc = function(radius){	//replace functionName with proper name for the function & enter the proper parameter 
 	//code to be run by the function
 }

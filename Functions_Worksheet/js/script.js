65c87fafd4f0fc812c528da94f696ce3c69fa61c
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-20 00:33:02 -0400

    this variable represents the circumference formula with the appropriate constants and variables listed

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -4,5 +4,5 @@
 
 	//basic skeleton set up for an anonymous function
 var circleCirc = function(radius){	//replace functionName with proper name for the function & enter the proper parameter 
-	//code to be run by the function
+	var circ = radius * 2 * 3.14;  //this variable represents the formula for finding the circumference of a circle.  The "2" and "3.14" are constant values.
 }

b5fdb6faa1de229f1bce68b7d1f40bb8dbdfe446
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-20 00:29:26 -0400

    this is the basic skeleton set up for an anonymous function

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,3 +1,8 @@
 //Janell Stultz 10-19-2013 Functions Worksheet
 
 //Circumference - calculate the circumference of a circle
+
+	//basic skeleton set up for an anonymous function
+var functionName = function(parameter){	
+	//code to be run by the function
+}

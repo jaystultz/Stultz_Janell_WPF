0fbec3f2e6d30c85005280f91417eec9b91f5e79
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-23 15:46:26 -0400

    add function with notes

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,3 +5,7 @@
 var transports = prompt("We are calculating if you wrote any run reports today.  \nHow many transports did you have today?"); //this variable represents how many transports were completed during my shift.
 var cancels = prompt("How many cancels did you have today?"); //this variable represents how many canceled calls I worked in a shift.
 
+var totalCalls = function(transports, cancels){ //this variable represents the function used to find out if there were run reports written.
+	var calls =  transports + cancels; //this variable represents the total number of calls regardless of whether they were transports or cancels.  
+}
+

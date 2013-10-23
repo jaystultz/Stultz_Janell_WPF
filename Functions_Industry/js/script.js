5390d5d39852652417f73c81f470868df1af2eac
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-23 15:45:57 -0400

    add explanation of problem and add givens with notes

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,7 @@
 //Janell Stultz 10/23/2013 - Assignment: Functions_Industry
 
+//In order to find out whether or not I have written any patient run reports today, I will answer the questions when prompted.
+//givens
+var transports = prompt("We are calculating if you wrote any run reports today.  \nHow many transports did you have today?"); //this variable represents how many transports were completed during my shift.
+var cancels = prompt("How many cancels did you have today?"); //this variable represents how many canceled calls I worked in a shift.
 

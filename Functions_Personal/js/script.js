40b2421f69d6c9720823a2551f9dff9a04a1df7e
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-23 15:32:49 -0400

    add var phoneAge and contractAge with prompts and notes

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,6 +1,8 @@
 //Janell Stultz 10/23/2013 - Assignment: Functions_Personal
 
 var yourAge = prompt("How old are you?"); //this variable represents your age.
+var phoneAge = prompt("How old is your phone?  Please enter the number of years you have had your current wireless device."); //this variable represents how long the user has had their current wireless phone.
+var contractAge = prompt("How long have you been in your current wireless contract?  Please enter the number of years."); //this variable represents how long the user has been in their current wireless contract.
 
 
 

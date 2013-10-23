71a8d56f747deba614bd366f71d1068c78d75152
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-23 15:49:22 -0400

    add variables for givens with prompts and notes

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,2 +1,5 @@
 //Janell Stultz 10/23/2013 - Assignment: Functions_Wacky
 
+var large = prompt ("How many large sized packages are you shipping?"); //this variable represents how many large packages are being shipped.
+var medium = prompt ("How many medium sized packages are you shipping?"); //this variable represents how many medium packages are being shipped.
+var small = prompt ("How many small sized packages are you shipping?"); //this variable represents how many small packages are being shipped.

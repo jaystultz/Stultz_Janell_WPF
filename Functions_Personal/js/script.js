bcdb28cd6f781100b46bc71da60b4131cfe12b59
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-23 15:34:07 -0400

    add function with vars yourAge, phoneAge, contractAge, upgrade and add return for upgrade with notes

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,7 +4,13 @@ var yourAge = prompt("How old are you?"); //this variable represents your age.
 var phoneAge = prompt("How old is your phone?  Please enter the number of years you have had your current wireless device."); //this variable represents how long the user has had their current wireless phone.
 var contractAge = prompt("How long have you been in your current wireless contract?  Please enter the number of years."); //this variable represents how long the user has been in their current wireless contract.
 
-
+var phoneUpgrade = function(yourAge, phoneAge, contractAge){ //this variable represents the "anonymous" function used to determine whether or not the user is eligible for a wireless phone upgrade.
+	var YourAge = 18; //this variable represents the minimum age required is 18 years old. 
+	var phoneAge = 2; //this variable represents the minimum phone age is 2 years old.
+	var contractAge = 2; //this variable represents the minimum contract age is 2 years.
+	var upgrade = yourAge && phoneAge && contractAge; //this variable represents that variables yourAge, phoneAge, and contractAge all have to be true in order for an upgrade to be available.
+	return upgrade; //this represents the "finished product" of the function
+}
 
 
 

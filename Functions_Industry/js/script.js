456be4bb55138a57314ee21d980f0b87c230a0fb
Janell Stultz <Jnellnell00@fullsail.edu>
2013-10-23 15:47:19 -0400

    add ternary operator with printouts, alerts and notes for function

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -11,3 +11,8 @@ var totalCalls = function(transports, cancels){ //this variable represents the f
 
 totalCalls(); //this represents the invocation of the function
 
+//this is an example of ternary operator
+(transports === "") ? console.log ("Please enter a valid number.") & alert("Please enter a valid number.") : //this if statement contains the validation statement for the clients variable meaning that this statement checks to ensure that the variable is not left blank.  If it is blank, an error alert is produced.
+(cancels === "") ? console.log("Please enter a valid number.") & alert("Please enter a valid number.") : //this if statement contains the validation statement for the clients variable meaning that this statement checks to ensure that the variable is not left blank.  If it is blank, an error alert is produced.
+totalCalls = (transports && cancels > 0) ? console.log("Yup, you wrote some run reports today.") & alert("Yup, you wrote some run reports today.") : console.log("Must have been a slow day because you didn't write any run reports.") & alert("Must have been a slow day because you didn't write any run reports."); //these are the printouts that will be seen depending on what answers are given in the prompts.  If calls > 0 then reports are written.  If calls < 0, then no reports are written.  
+
